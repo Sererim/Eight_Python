f714c8bc18d0af2a8601de75af09578e39c7174e
Sererim <danielleymanw@gmail.com>
2023-08-11 11:44:22 +0300

    Basic skeleton for functions.

---
+++ homework/home.py
@@ -0,0 +1,18 @@
+def walk_dir() -> None:
+    pass
+
+
+def save_to_json() -> None:
+    pass
+
+
+def save_to_csv() -> None:
+    pass
+
+
+def save_to_pickle() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    pass
